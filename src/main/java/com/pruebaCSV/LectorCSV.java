50c00ddb29fd675b37d99dafaf0641258c88f02b
Gian08922 <giancarloriosaltamirano@hotmail.com>
2022-10-19 21:47:39 +0200

    Añadidas las clases de PruebaCSV y LectorCSV. No he probado nada uwu

---
+++ src/main/java/com/pruebaCSV/LectorCSV.java
@@ -0,0 +1,31 @@
+package com.pruebaCSV;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/*
+Esta clase se encarga de leer el archivo .csv que se le pase como parametro
+
+No devuelve nada, si no que muestra el contenido del csv por el terminal
+ */
+
+public class LectorCSV {
+    /**
+     * @author Giancarlo Rios
+     */
+    public void leeCSV(String nombreArchivo) {
+        try {
+            CSVReader csvReader = new CSVReader( new FileReader(nombreArchivo));
+            String[] fila;
+            while((fila = csvReader.readNext()) != null){
+                System.out.println(fila[0] + " | " + fila[1] + " | " + fila[2] + " | ");
+            }
+            csvReader.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/pruebaCSV/PruebaCSV.java
@@ -0,0 +1,18 @@
+package com.pruebaCSV;
+
+/*
+    Ejemplo muy sencillo de acceso a un archivo .csv desde java
+    NO UTLIZA ningun gestor de dependencias
+ */
+
+public class PruebaCSV {
+    /**
+     * @author Giancarlo Rios
+     */
+
+
+    public static void main(String[] args) {
+        LectorCSV miLector = new LectorCSV();
+        miLector.leeCSV("./datos/pokemon.csv");
+    }
+}
